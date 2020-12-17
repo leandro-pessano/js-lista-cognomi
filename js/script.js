f8c09d23c87c9d3576261eaa4493ba565682ed5d
Leandro <thesquallrock@gmail.com>
2020-12-17 17:01:17 +0100

    un po' di ordine

--- js/script.js
+++ js/script.js
@@ -4,15 +4,18 @@
 // stampa la lista ordinata alfabeticamente (occhio alle maiuscole, se volete potete convertire il nome prima inserirlo).
 // Scrivi anche la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova
 
+// Nome delle variabili
 var listaCognomi, inputCognome, toUp, toLow;
 
-listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi']
+// Definizione variabili
+listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 inputCognome = prompt('Inserisci il tuo cognome');
 toUp = inputCognome.charAt(0).toUpperCase();
 toLow = inputCognome.substring(1).toLowerCase();
 inputCognome = toUp + toLow;
 listaCognomi.push(inputCognome);
+
+// Stampa
 console.log(listaCognomi);
-listaCognomi.sort();
-console.log(listaCognomi);
+console.log(listaCognomi.sort());
 console.log(listaCognomi.indexOf(inputCognome) + 1);
